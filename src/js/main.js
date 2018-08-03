ca10b993a414ca9f717682b77c21ba4bfc74b4f9
Miranda Naranjo <m.naranjo.medel@gmail.com>
2018-08-03 12:15:21 -0400

    cambios

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,28 @@ btnPerfil.addEventListener('click', () => {
   paperPerfil.style.display = 'block';
   paperBotons.style.display = 'none';  
 });
+
+// Login con mail y password
+
+btnLogin.addEventListener('click', () => {
+  const email = document.getElementById('inputEmail').value;
+  const password = document.getElementById('inputPassword').value;
+  firebase.auth().signInWithEmailAndPassword(email, password)
+
+    .catch((error) => {
+      const inputEmailUser = document.getElementById('inputEmail');
+      inputEmailUser.value = '';
+      const inputPasswordUser = document.getElementById('inputPassword');
+      inputPasswordUser.value = '';
+      const alertLogin = document.getElementById('alertPassword');
+      const msjErrorFirebase = error.message;
+      if (msjErrorFirebase === 'The email address is badly formatted.') {
+        alertLogin.innerHTML = '<div class="alert alert-danger alertConteiner" role="alert"> Error: Por favor ingresa un correo eléctronico válido</div>';
+      } else if (msjErrorFirebase === 'The password is invalid or the user does not have a password.') {
+        alertLogin.innerHTML = '<div class="alert alert-danger alertConteiner" role="alert"> Error: Password Invalido, Ingrese un password de 6 o más caracteres </div>';
+      }
+      console.log('Error de Firebase > ' + error.code);
+      console.log('Error de Firebase > mensaje' + error.message);
+    });
+}); 
+// fin evento click del boton login normal
