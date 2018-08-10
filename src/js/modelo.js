94495c436aa8e3e809e817dc370ab11b7054ad74
Miranda Naranjo <m.naranjo.medel@gmail.com>
2018-08-10 11:07:44 -0400

    termine por ahora

--- src/js/modelo.js
+++ src/js/modelo.js
@@ -51,6 +51,7 @@ window.calculationTaf = () => {
       });
   };
 
+  /*
   window.calculationTaf = (resultResta) => {
      const cost = 680;
      const saldoBip = 1000;
@@ -58,5 +59,5 @@ window.calculationTaf = () => {
        return true;
      }       
   };
-
+*/
   
